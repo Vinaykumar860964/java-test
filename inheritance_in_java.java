cc4220d02ef04099f108969ce9c4a0f8841473bd
Vinaykumar860964 <122643407+Vinaykumar860964@users.noreply.github.com>
2023-01-14 02:33:36 +0530

    Add files via upload

---
+++ inheritance_in_java.java
@@ -0,0 +1,30 @@
+
+class father{
+
+    public void fatherinfo(){
+
+        System.out.println("my father is everything for me");
+    }
+
+class mother extends father();
+
+    public void motherinfo()
+    {
+        System.out.println(" my mother is my love");
+    }
+}
+}
+
+
+public class inheritance_in_java {
+
+    public static void main(String[] args) {
+        
+
+        mother m=new mother();
+        m.motherinfo
+    }
+    
+
+
+}
