ca658dba5bd5425bf0056b21bd66d91b9521f7e8
Vinaykumar860964 <122643407+Vinaykumar860964@users.noreply.github.com>
2023-01-14 02:09:39 +0530

    Add files via upload
    this is java code in array

---
+++ array1.java
@@ -0,0 +1,10 @@
+public class array1 {
+    public static void main(String[] args) {
+        int a=233;
+        int b=33;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        }
+}
